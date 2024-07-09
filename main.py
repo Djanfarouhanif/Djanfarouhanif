62ebf09d0a9cb70ced5d52bf4dde4ba56e12ba1f
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-09 22:54:48 +0000

    add player point

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
 game = False
-counter = 3
+counter = 2
+point = 0
+n = [ 1, 2, 3, 4, 5, 6]
+number_mystic = random.choice(n)
 while game == False:
-    n = [ 1, 2, 3, 4, 5, 6]
+
     entre = input("debut du jeux choisie un nombre  au hasard entre 1 et 6 : ")
-    number_mystic = random.choice(n)
+
     try:
         entre_int = int(entre)
         if entre_int <= 0 or entre_int >6:
@@ -13,12 +16,15 @@ while game == False:
 
             if number_mystic == entre_int:
                 print("Felicitation du a gagner")
+                point += 2
+                print(f"\nVous avez {point} point")
+                number_mystic  = random.choice(n)
             else:
                 if counter !=0:
                     counter -= 1
-                    print(f"Il vous reste {counter} point")
+                    print(f"\nIl vous reste {counter} chance")
                 if counter == 0:
-                    print("Fin du jeux")
+                    print(f"\nFin du jeux vous avez accumiler {point}  point")
                     break
     except:
         print("Veuiller utiliser les chiffre ")
