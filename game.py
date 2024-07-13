96959ceaf24f675f602f6d4609d38619d605aee2
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-13 13:37:19 +0000

    player move possition

--- game.py
+++ game.py
@@ -23,11 +23,30 @@ class Game:
         #dessiner le group de calque
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer=3)
         self.group.add(self.player)
+    def handle_input(self):
+        pressed  = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation("up")
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation("down")
+        elif pressed[pygame.K_LEFT]:
+            
+            self.player.move_left()
+            self.player.change_animation("left")
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation("right")
+
 
     def run(self):
+        clock = pygame.time.Clock()
         #Boucle du jeu
         running = True
         while running:
+            self.handle_input()
             self.group.update()
             self.group.center(self.player.rect)
             self.group.draw(self.screen)
@@ -36,5 +55,6 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
 
+            clock.tick(60)
 
         pygame.quit()
