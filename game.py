4afeb840d21d4c732aa5dc84b67c41cf45123631
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-13 12:32:06 +0000

    add player

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import  pygame
 import pytmx
 import pyscroll
-
+from player import Player
 
 class Game:
     def __init__(self):
@@ -15,18 +15,21 @@ class Game:
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
        
+       #generer un joueur
+        player_postion = tmx_data.get_object_by_name("player")
+        self.player = Player(player_postion.x, player_postion.y)
+        
+
         #dessiner le group de calque
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer=3)
+        self.group.add(self.player)
 
     def run(self):
-
-
         #Boucle du jeu
-
         running = True
-
-
         while running:
+            self.group.update()
+            self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
             for event in pygame.event.get():
